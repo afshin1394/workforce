88161562774bb248998912b56af7a41a7d57fb92
mohammadparsa.d <mohammadparsa.d@mtnirancell.ir>
2024-09-18 15:48:07 +0330

    chore: change base url

--- composeApp/src/commonMain/kotlin/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/di/Koin.kt
@@ -194,7 +194,7 @@ fun httpModule() = module {
             }
             configure()
             defaultRequest {
-                url(ProductionBASEURL)
+                url(DeploymentBASEURL)
                 contentType(ContentType.Application.Json)
                 headers {
                     append(
@@ -232,7 +232,7 @@ fun httpModule() = module {
             }
             configure()
             defaultRequest {
-                url(ProductionBASEURL)
+                url(DeploymentBASEURL)
                 contentType(ContentType.Application.Json)
                 headers {
 
