1ff6e75d2b4c28f0930ffe4873434cfb83b5fef2
mohammadparsa.d <mohammadparsa.d@mtnirancell.ir>
2024-09-27 15:58:02 +0330

    fix: open the location in a desired map application

--- composeApp/src/androidMain/kotlin/irancell/nwg/wfm/OpenInMap.kt
+++ composeApp/src/androidMain/kotlin/irancell/nwg/wfm/OpenInMap.kt
@@ -1,20 +1,29 @@
 package irancell.nwg.wfm
 
+import android.R
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
-import android.util.Log
+import java.util.Locale
 
 
+@SuppressLint("DefaultLocale")
 actual fun openInMap(lat: String, long: String) {
-    val geoUri = "http://maps.google.com/maps?q=loc:$lat,$long"
-    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(geoUri))
+    try {
+        val uri = java.lang.String.format(
+            Locale.ENGLISH,
+            ((((("geo:" + lat).toString() + "," + long).toString() + "?q=" + lat).toString() + "," + long).toString() + " (" + "").toString() + ")"
+        )
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+        intent.setFlags(FLAG_ACTIVITY_NEW_TASK)
+        val context = provideAppContext() as Context;
 
-    intent.setFlags(FLAG_ACTIVITY_NEW_TASK)
-
-    val context = provideAppContext() as Context;
-    if (intent.resolveActivity(context.packageManager) != null) {
-        context.startActivity(intent)
+        if (intent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(intent)
+        }
+    } catch (ex: ActivityNotFoundException) {
     }
 }
--- composeApp/src/commonMain/kotlin/presentation/screens/main/viewmodel/MainScreenVM.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/main/viewmodel/MainScreenVM.kt
@@ -1,6 +1,5 @@
 package presentation.screens.main.viewmodel
 
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
