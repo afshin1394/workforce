bc7be238fb850f4c0137e07bb5cd405b61456430
afshin <afshinsamiei94@gmail.com>
2024-10-26 00:11:59 -0700

    bug fix on auto update

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -172,8 +172,8 @@ android {
         applicationId = "irancell.nwg.wfm"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 20
-        versionName = "0.2.0"
+        versionCode = 21
+        versionName = "0.2.1"
     }
     packaging {
         resources {
