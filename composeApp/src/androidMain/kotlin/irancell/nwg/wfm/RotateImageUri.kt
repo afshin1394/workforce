37edfa4a5c2a09cd032b4b9d2dfff7f08bab7479
marzieh.ye <marzieh.ye@mtnirancell.ir>
2024-11-13 11:20:05 +0330

    fix RotateImageUri

--- composeApp/src/androidMain/kotlin/irancell/nwg/wfm/RotateImageUri.kt
+++ composeApp/src/androidMain/kotlin/irancell/nwg/wfm/RotateImageUri.kt
@@ -9,7 +9,6 @@ import android.provider.MediaStore
 actual fun RotateImageUri(originalUri: String,key:String,angle: Float): String {
 
     val uri = Uri.parse(originalUri)
-    println("checkAngle   ${(angle  )}")
     val rotatedUri = rotateImageAndSave(uri,key, angle)
     return rotatedUri.toString()
 }
@@ -17,28 +16,20 @@ actual fun RotateImageUri(originalUri: String,key:String,angle: Float): String {
 private fun rotateImageAndSave(uri: Uri, key: String, angle: Float): Uri {
     val context = provideAppContext() as Context
     var rotatedUri: Uri? = null
-
     val bitmap = MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
-
     try {
         val matrix = Matrix()
-        matrix.postRotate(angle.toFloat())
-        println("checkAngle   ${(angle  )}")
+        matrix.postRotate(angle)
 
         val rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
-        println("checkAngle   ${(matrix  )}")
-
         val savePath = InternalStorage.getProcessRouteEdited(context)
 
         rotatedUri = SaveBitmapToFile(savePath, key = key, rotatedBitmap) as Uri
-        println("checkAngle   ${(rotatedUri  )}")
         rotatedBitmap.recycle()
 
     } finally {
         bitmap.recycle()
     }
-    println("checkAngle   ${(uri  )}")
-    println("checkAngle   ${(rotatedUri  )}")
 
     return rotatedUri ?: uri
 }
